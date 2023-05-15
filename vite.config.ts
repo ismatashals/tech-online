d50b241c2248f87450b55e99da35409f7bd9cdac
Islam <ueaten04@gmail.com>
2023-05-15 08:13:29 +0600

    added aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "icons": path.resolve(__dirname, "./src/assets/icons"),
+      "images": path.resolve(__dirname, "./src/assets/images"),
+      "components": path.resolve(__dirname, "./src/components"),
+    }
+  }
 })
